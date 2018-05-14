6c8cec56027fe46c4d651b3075b52d7094ee0372
Urmel91 <vanselow@muk.uni-hannover.de>
2018-05-14 16:01:17 +0200

    unzip hinzugefügt

---
+++ unzip_data.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+for myfile in Downloads/ras*.tar; do
+    new_folder="${myfile%.*}/"
+    mkdir -p ${new_folder}
+    tar -xf ${myfile} -C ${new_folder}
+    for zipfile in ${new_folder}/*.gz; do
+        gzip -d ${zipfile} 
+    done
+    echo "."
+done
