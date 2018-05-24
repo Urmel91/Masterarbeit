5ca305828740356c5c37743787a98fabe1d28534
Urmel91 <vanselow@muk.uni-hannover.de>
2018-05-24 14:55:33 +0200

    netcdf-sh akt

--- netcdf_read.sh
+++ netcdf_read.sh
@@ -1,42 +1,40 @@
 #!/bin/bash
 
+pat=~/Schreibtisch/Masterarbeit/Originaldaten/${1}/${2}/${3}/${4}/
 
+phist=${pat}hist/
+prcp=${pat}rcp85/
 
-choose_path() {
-  if [[ "${1}" =~ "cnrm" ]]; then
-    if [[ "${2}" =~ "cclm" ]]; then
-#      if [[ "${3}" =~ "hist" ]]; then
-	phist=~/MA/Originaldaten/CNRM/cclm/hist/
-#      elif [[ "${3}" =~ "rcp" ]]; then
-	prcp=~/MA/Originaldaten/CNRM/cclm/rcp85/
-#      fi
-    fi
-  fi
-}
+if [[ ${5} =~ "r" ]]; then
 
-choose_path ${1} ${2} #${3}
+    echo "reading hist data..."
 
-echo "reading hist data..."
+    count=0
+    for infile in ${phist}${2}*.nc; do
+        outfile=${infile%.nc}
+        gfortran-6 netcdf_read.f90 -o netcdf_read -I/usr/local/netcdf/include -L/usr/local/netcdf/lib/ -lnetcdf -lnetcdff    
+#       ifort netcdf_read.f90 -o netcdf_read -I/muksoft/packages/netcdf/4_intel/include/ -L/muksoft/packages/netcdf/4_intel/lib64/ -lnetcdf -lnetcdff
+        ./netcdf_read ${infile} ${count} ${#outfile} ${outfile} ${2}
+        ((count++))
+    done
 
-count=0
-for infile in ${phist}pr*.nc; do
-    outfile=${infile%.nc}
-    ifort netcdf_read.f90 -o netcdf_read -I/muksoft/packages/netcdf/4_intel/include/ -L/muksoft/packages/netcdf/4_intel/lib64/ -lnetcdf -lnetcdff
-    ./netcdf_read ${infile} ${count} ${#outfile} ${outfile}
-    ((count++))
-done
+    echo "reading rcp85 data..."
 
-echo "reading rcp85 data..."
+    count=0
+    for infile in ${prcp}${2}*.nc; do
+        outfile=${infile%.nc}
+        gfortran-6 netcdf_read.f90 -o netcdf_read -I/usr/local/netcdf/include -L/usr/local/netcdf/lib/ -lnetcdf -lnetcdff        
+#       ifort netcdf_read.f90 -o netcdf_read -I/muksoft/packages/netcdf/4_intel/include/ -L/muksoft/packages/netcdf/4_intel/lib64/ -lnetcdf -lnetcdff
+        ./netcdf_read ${infile} ${count} ${#outfile} ${outfile} ${2}
+        ((count++))
+    done
 
-count=0
-for infile in ${prcp}pr*.nc; do
-    outfile=${infile%.nc}
-    ifort netcdf_read.f90 -o netcdf_read -I/muksoft/packages/netcdf/4_intel/include/ -L/muksoft/packages/netcdf/4_intel/lib64/ -lnetcdf -lnetcdff
-    ./netcdf_read ${infile} ${count} ${#outfile} ${outfile}
-   ((count++))
-done
+    rm -r ${phist}*.nc
+    rm -r ${prcp}*.nc
+fi
 
-echo "concat data..."
+if [[ ${5} =~ "c" ]] || [[ ${6} =~ "c" ]]; then
+    echo "concat data..."
 
-./concat.py ${phist} ${prcp}
-
+    ./concat.py ${phist} ${prcp}
+fi
